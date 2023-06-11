75625570bc2814883da004377839c654f1c40864
Robin <robin@gocrazy.cn>
2023-06-12 00:27:27 +0800

    add logger in chat_management

--- app/core/chat_management.py
+++ app/core/chat_management.py
@@ -3,6 +3,7 @@ from app.core.model.llm_model import LLMModel
 from app.core.prompt_management import PromptManagement
 from app.core.vector_store_management import VectorStoreManagement
 from app.core.torch_management import torchMngr
+from app.helper.logger import logger
 
 
 class ChatManagement: 
@@ -18,13 +19,21 @@ class ChatManagement:
         self.vsMngr = VectorStoreManagement()
     
     def chat(self, query: str, history: list) -> tuple:
+        logger.info(f"Chatting with query: {query}")
+        logger.info("loading vector store")
         self.vsMngr.load("LaborLaw", self.embedding)
+        
+        logger.info("searching for similar documents")
         related_docs_with_score = self.vsMngr.similarity_search(query, k=10)
         torchMngr.gc()
         
+        logger.info("generating prompt")
         prompt = self.prompt_mngr.generate_prompt(related_docs_with_score, query)
+        logger.info(f"prompt: {prompt}")
         
+        logger.info("chatting with llm model")
         response, history = self.llm_model.chat(prompt, history)
+        logger.info(f"answer: {response}")
         torchMngr.gc()
         return response, history
     
